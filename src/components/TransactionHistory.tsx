4ff18c9f5d09cfa8a0bcf9b4fee02edb16d16390
rflrytdzn <kuhaku.blank.rd@gmail.com>
2025-07-20 14:55:23 +0800

    fix: tx history ui bug

--- src/components/TransactionHistory.tsx
+++ src/components/TransactionHistory.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 import { useState } from "react";
 import TransactionDetails from "./TransactionDetails";
-import CashinIcon from "../../public/cashin2.png"
-import BackIcon from "../../public/BackIcon.svg"
+import CashinIcon from "../../public/cashin2.png";
+import BackIcon from "../../public/BackIcon.svg";
 interface Transaction {
   id: string;
   status: string;
@@ -20,46 +20,43 @@ interface TransactionProps {
   onClose: () => void;
 }
 
-
 const TransactionHistory = ({ data, onClose }: TransactionProps) => {
-    const [isModalOpen, SetIsModalOpen] = useState<boolean>(false);
-    const [selectedTransaction, setSelectedTransaction] = useState<Transaction | null>(null);
+  const [isModalOpen, SetIsModalOpen] = useState<boolean>(false);
+  const [selectedTransaction, setSelectedTransaction] =
+    useState<Transaction | null>(null);
 
-    const handleModal = (tx: Transaction) => {
-        SetIsModalOpen(true);
-        setSelectedTransaction(tx);
-    };
+  const handleModal = (tx: Transaction) => {
+    SetIsModalOpen(true);
+    setSelectedTransaction(tx);
+  };
 
   return (
     <div>
-      <div className=" flex flex-col  bg-white   rounded-lg w-96 absolute z-50 h-screen overflow-y-auto">
+      <div className=" flex flex-col  bg-white   rounded-lg w-screen absolute z-50  overflow-y-auto">
         <div className="p-4">
           <div className="mb-4 flex items-center justify-between">
-  <button onClick={onClose}>
-    <BackIcon />
-  </button>
-
-  <h5 className="flex-1 text-center text-slate-800 text-lg font-semibold">
-    Transaction History
-  </h5>
-
-  {/* Empty element to balance the layout */}
-  <div className="w-8" /> {/* Same width as the back button */}
-</div>
+            <button onClick={onClose}>
+              <BackIcon />
+            </button>
+            <h5 className="flex-1 text-center text-slate-800 text-lg font-semibold">
+              Transaction History
+            </h5>
+            {/* Empty element to balance the layout */}
+            <div className="w-8" /> {/* Same width as the back button */}
+          </div>
           <div className="divide-y divide-slate-200">
             {data.map((tx, index) => (
               <div
                 key={index}
                 className="flex items-center justify-between pb-3 pt-3 last:pb-0"
-                onClick={()=>handleModal(tx)}
+                onClick={() => handleModal(tx)}
               >
-                <TransactionDetails data={selectedTransaction!} isOpen={isModalOpen} onClose={()=> SetIsModalOpen(false)}/>
+                
                 <div className="flex items-center gap-x-3">
                   <Image
                     src={CashinIcon}
-                    alt='cashinicon'
+                    alt="cashinicon"
                     className="relative inline-block h-8 w-8 rounded-full object-cover object-center"
-
                   />
                   <div>
                     <h6 className="text-slate-800 font-semibold">
@@ -71,6 +68,11 @@ const TransactionHistory = ({ data, onClose }: TransactionProps) => {
                 <h6 className="text-slate-600 font-medium">{tx.timestamp}</h6>
               </div>
             ))}
+            <TransactionDetails
+                  data={selectedTransaction!}
+                  isOpen={isModalOpen}
+                  onClose={() => SetIsModalOpen(false)}
+                />
           </div>
         </div>
       </div>
